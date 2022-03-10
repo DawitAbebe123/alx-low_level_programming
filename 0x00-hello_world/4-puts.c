8f5af680fa48acd4b21c12bc12abb31f6435a849
dawitabebe <daveabebe0726@gmail.com>
2022-03-10 10:51:40 -0800

    C program that prints a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
-/**
+
+/*
  *
- *  * main - Entry point
+ *  * main - entry point
  *
- *   *
+ */**
+
+ * main - Entry point
+ *
+ *  *
+ *
+ *   * Return: always 0(success)
  *
  *    * Return: always 0 (success)
  *
  *     */
 
-int main(void)
+
+
+ int main(void)
+
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
 }
